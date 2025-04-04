32933e4468dfb775c434fead3c7ac14d7812735f
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-04-04 19:57:40 +0200

    Initial commit

---
+++ projects/PROJECT_NAME/src/public_api.ts
@@ -0,0 +1,2 @@
+/* eslint-disable unicorn/filename-case */
+export * from './lib/library.module';
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+// import { LibraryModule } from '%PROJECT_NAME%';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    /* import your library modules/components here */
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
